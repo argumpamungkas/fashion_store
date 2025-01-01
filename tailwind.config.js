e6679ac21983e3682aa8a1ef0927269fe93e0dd1
Argumelar Pamungkas <argump.12@gmail.com>
2025-01-01 18:13:17 +0700

    Add navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Satoshi", "cursive"],
+      },
+      colors: {
+        primary1: "#6875F5",
+        primary2: "#5145CD",
+        primary3: "#42389D",
+        bgColor: "#CDDBFE",
+        bgBorder: "#1A56DB",
+      },
+    },
   },
   plugins: [],
-}
-
+};
